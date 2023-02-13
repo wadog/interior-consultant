1cc7a087dd8000224451f47eba4e81c009fc146f
walter <walthermaximo@gmail.com>
2023-02-13 17:05:39 -0300

    arreglo de css web & mobile

--- js/menuDesplegable.js
+++ js/menuDesplegable.js
@@ -1,11 +1,17 @@
 let btn = document.querySelector('button');
+let span = document.querySelector('span');
 let menuDesplegable = document.querySelector('.menuDesplegable');
-let section =  document.querySelector('section')
-console.log(section);
+let section =  document.querySelector('section');
+let logoInicio = document.querySelector('.navbar-brand');
 
 btn.addEventListener('click', ()=>{
-    console.log('aprieto boton')
-    menuDesplegable.classList.toggle('mostrar')
-    section.classList.toggle('ocultar')
-    console.log(menuDesplegable)
+    if(span.innerHTML === 'close'){
+        span.innerHTML = 'menu';
+        console.log(span.innerHTML)
+    }else{
+        span.innerHTML = 'close';
+    }
+    menuDesplegable.classList.toggle('mostrar');
+    section.classList.toggle('ocultar');
+    logoInicio.classList.toggle('ocultar');
 })
